e809cfca0adb48fed0d8a631691a77a23b2f46ad
Sindre Stephansen <sindre@sindrestephansen.com>
2019-09-17 13:58:28 +0200

    Create the EffectBox module

---
+++ src/main/scala/EffectBox.scala
@@ -0,0 +1,17 @@
+package EffectBox
+
+import chisel3._
+
+
+/**
+  * The main class for the effect box.
+  * It takes input from the outside, puts it
+  * through the effect modules, and outputs it.
+  */
+class EffectBox extends Module {
+  val io = IO(new Bundle {
+
+  })
+
+  // val effect = Module(new Effect)
+}
