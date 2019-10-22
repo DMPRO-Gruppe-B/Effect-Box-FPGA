c3fc4bd2182cab65342f2ca67d68a2612157a370
Henriette Andersen <henriean@stud.ntnu.no>
2019-10-22 17:00:49 +0200

    Add module for signal mixing.

---
+++ src/test/scala/signalOperations/MixSignalsSpec.scala
@@ -0,0 +1,55 @@
+package signalOperations
+
+import chisel3._
+import chisel3.iotesters.PeekPokeTester
+import org.scalatest.{Matchers, FlatSpec}
+
+
+class MixSignalsSpec extends FlatSpec with Matchers {
+  import MixSignalsTest._
+  behavior of "MixSignalsSpec"
+
+  it should "" in {
+    chisel3.iotesters.Driver(() => new MixSignals()) { c =>
+      new MixSpecificSignals(c)
+    } should be(true)
+  }
+
+}
+
+
+object MixSignalsTest {
+
+  class MixSpecificSignals(c: MixSignals) extends PeekPokeTester(c) {
+
+    val signal1_list : List[BigInt] = List(47483648, -47806, 900867564, 6438758, 824, 0, 2147483647)
+    val signal2_list : List[BigInt] = List(54624633, 432452, -664538086, 0, 344, 50, 567391956)
+    val numerator_list : List[BigInt] = List(1, 5, 12, 8, 16, 0, 1)
+    val denominator_list : List[BigInt] = List(5, 7, 56, 8, 70, 1, 5)
+
+    for (ii <- 0 until signal1_list.length) {
+      val a = signal1_list(ii)
+      val b = signal2_list(ii)
+      val n = numerator_list(ii)
+      val d = denominator_list(ii)
+
+      poke(c.io.operand1, a.S(32.W))
+      poke(c.io.operand2, b.S(32.W))
+      poke(c.io.numerator1, n.U(8.W))
+      poke(c.io.denominator1, d.U(8.W))
+      val result = peek(c.io.result)
+
+      val exact = (a*n + b*(d-n))/d
+
+      println("Mixing signals:_")
+      println("Expected " + exact + ", and got " + result)
+
+      expect(c.io.result, exact)
+
+
+    }
+
+
+  }
+
+}
