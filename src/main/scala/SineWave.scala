aa1d530059c35492fcdb26b7b7d45b6d32cfbdbb
Ole Ravna <olevra@stud.ntnu.no>
2019-10-24 14:38:14 +0200

    Add sine wave

---
+++ src/main/scala/SineWave.scala
@@ -0,0 +1,27 @@
+package EffectBox
+
+import chisel3._
+import chisel3.util._
+
+
+class SineWave extends Module{
+
+  val io = IO(new Bundle {
+    val inc = Input(Bool())
+    val signal = Output(new SignedDoubleFraction)
+
+  })
+
+  val (x, wrap) = Counter(io.inc, 180)
+  val (n, _) = Counter(wrap, 2)
+
+  val top = WireInit(4.U * x * (180.U - x))
+  val bot = WireInit(40500.U - x * (180.U - x))
+
+
+  io.signal.denominator := bot
+  io.signal.numerator := top
+  io.signal.negative := n
+
+
+}
