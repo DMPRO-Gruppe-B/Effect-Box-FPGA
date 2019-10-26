2ca3ba0cf07fbcadd190194eec63acd58fb2691f
Michael Allport <mmallport@gmail.com>
2019-10-27 00:30:30 +0200

    Added DAC interface tests

---
+++ src/test/scala/DACInterfaceSpec.scala
@@ -0,0 +1,48 @@
+package EffectBox
+
+import chisel3._
+import chisel3.iotesters.PeekPokeTester
+import org.scalatest.{Matchers, FlatSpec}
+import chisel3.util._
+import scala.util.control.Breaks._
+
+class DACInterfaceSpec extends FlatSpec with Matchers {
+  import DACTest._
+
+  behavior of "DAC Spec"
+
+  it should "Output a sample equivalent to the input bits" in {
+    chisel3.iotesters.Driver
+      .execute(Array("--backend-name", "treadle"), () => new DACInterface) {
+        b =>
+          new DACRead(b)
+      } should be(true)
+  }
+}
+
+object DACTest {
+  class DACRead(b: DACInterface) extends PeekPokeTester(b) {
+    import scala.math.abs
+    import java.io.PrintWriter
+    import scala.io.Source
+
+    val filename = "sound.txt"
+
+      for (line <- FileUtils.getLines(filename)) {
+        poke(b.io.LRCLK, true)
+        poke(b.io.sample, line.toInt)
+        poke(b.io.enable, true)
+
+        for (bit <- (TestUtils.toBinaryString(line.toInt, 16))) {
+            step(1)
+            expect(b.io.bit,bit.toInt)
+        }
+        poke(b.io.LRCLK, false)
+        for (i <- 1 to 16) {
+          poke(b.io.sample, 0.S)
+          step(1)
+          expect(b.io.enable,false)
+        }
+    }
+  }
+}
