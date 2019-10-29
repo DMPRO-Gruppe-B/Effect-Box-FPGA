dfb25aff92f443b3d069ee9b7b4638f36ce33e8c
Michael Allport <mmallport@gmail.com>
2019-10-29 13:59:09 +0100

    Re-added Fraction and FractionReduce in the new file FractionHelpers

--- src/main/scala/Encapsulators.scala
+++ src/main/scala/Encapsulators.scala
@@ -4,21 +4,6 @@ import chisel3._
 import scala.collection.BitSet
 import chisel3.core.Wire
 
-
-class Fraction extends Bundle(){
-    val numerator = UInt(8.W)
-    val denominator = UInt(8.W)
-}
-
-object Fraction {
-    def nop: Fraction = {
-      val b = Wire(new Fraction)
-      b.numerator   := 0.U
-      b.denominator := 0.U
-      b
-    }
-  }
-
 object Multiply{
     def apply(numerator: UInt,denominator: UInt, number: SInt) : SInt = {
         val m = Module(new Multiply)
@@ -115,35 +100,3 @@ class FallingEdge extends Module{
       })
     io.out := !io.signal && RegNext(io.signal)
 }
-
-class FractionReduce(fractionInput : Double) extends Bundle(){
-    var localFraction = fractionInput
-    var denom : Int = 1
-    var num   : Int = 1
-
-    while (localFraction < scala.math.abs(1)){
-        localFraction *= 10
-        denom         *= 10
-    }
-
-    val gcd : Int = gcd(localFraction.toInt,denom.toInt)
-
-    denom /= gcd
-    num = (localFraction.toInt)/gcd
-
-    // This is dumb
-    try{
-        denom.toByte
-        num.toByte
-    }
-    catch{
-        case _: Throwable => println("Fraction could not be represented as 8 bit")
-    }
-
-    val numUInt = num.asUInt
-    val denomUInt = denom.asUInt
-
-    def gcd(a: Int,b: Int): Int = {
-        if(b ==0) a else gcd(b, a%b)
-     }
-  }
