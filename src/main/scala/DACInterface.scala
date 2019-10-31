418ea9c2ce11e6ca8564dcdc5967d03d28af64fd
Michael Allport <mmallport@gmail.com>
2019-10-31 17:06:22 +0100

    Moved enable signal in DAC to only work in the first cycle, and updated test to reflect that

--- src/main/scala/DACInterface.scala
+++ src/main/scala/DACInterface.scala
@@ -17,21 +17,20 @@ class DACInterface extends Module {
 
   val sample_reg = RegInit(UInt(16.W), 0.U)
 
+  io.enable := false.B
+
   io.bit_left := sample_reg(15)
 
   // Disable right channel (for now)
   io.bit_right := 0.U
 
   when(io.LRCLK) { // LRCLK high
-    io.enable := true.B
     when(RisingEdge(io.LRCLK)) { // LRCLK rising edge
+      io.enable := true.B
       sample_reg := io.sample.do_asUInt
     }
     .otherwise{
         sample_reg := sample_reg << 1
     }
-  }
-  .otherwise{
-      io.enable := false.B
   }
 }
