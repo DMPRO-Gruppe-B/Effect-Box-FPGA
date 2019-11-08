a4284a2b9b5769e021990c1977dc07729490544c
Aksel Slettemark <akselslettemark@gmail.com>
2019-11-08 01:42:18 +0100

    Structure effect control in EffectControl.

--- src/main/scala/SPITest.scala
+++ src/main/scala/SPITest.scala
@@ -14,29 +14,19 @@ class SPITest extends Module {
   })
 
   withReset(!reset.asBool()) {
-    val ledreg = RegInit(0.U(4.W))
 
-    val slave = Module(new SPISlave)
-    slave.io.spi <> io.spi
-
-    val config = RegInit(VecInit(Seq.fill(2)(0x0.U(16.W))))
-
-    when(slave.io.output_valid) {
-      val bytes = slave.io.output
-      val addr: UInt = bytes(23, 16)
-      val data: UInt = bytes(15, 0)
-      config(addr) := data
-
-      ledreg := slave.io.output(3, 0)
-    }
+    val effectControl = Module(new EffectControl)
+    effectControl.spi <> io.spi
 
     val bitCrush = Module(new BitCrush)
-    bitCrush.ctrl.bypass := config(0) & 1.U(1.W)
-    bitCrush.ctrl.nCrushBits := config(1) & 0xF.U(4.W)
+    bitCrush.ctrl <> effectControl.bitcrush
     bitCrush.io.dataIn := 0.S
 
-    val d = slave.io.debug
-    io.pinout := Vec(io.spi.cs_n, slave.io.output_valid, false.B, slave.io.output_valid, false.B, false.B, false.B, false.B, false.B, false.B, false.B, false.B, d(2), d(0), io.spi.cs_n, slave.io.output_valid).asUInt() // 0.U
+    val ledreg = RegInit(0.U(4.W))
+    when (effectControl.debug.slave_output_valid) {
+      ledreg := effectControl.debug.slave_output(3, 0)
+    }
     io.led := ledreg
+    io.pinout := Vec(io.spi.cs_n, false.B, false.B, effectControl.debug.slave_output_valid).asUInt()
   }
 }
