6dda062489243cc6c3b0b105555dda631a8e0fdf
Sindre Stephansen <sindre@sindrestephansen.com>
2019-11-18 15:47:03 +0100

    Minor delay rewrite and cleanup

--- src/main/scala/DelayBuffer.scala
+++ src/main/scala/DelayBuffer.scala
@@ -4,23 +4,23 @@ import chisel3._
 import blackboxes.BRAM
 
 class DelayBuffer extends Module {
-  val io = IO(new EffectBundle {
+  val io = IO(new Bundle {
+    val in           = Input(Sample())
+    val out          = Output(Sample())
+    val enable       = Input(Bool())
     val delaySamples = Input(UInt(16.W))
   })
 
   val addr_width = 17
-  val mem  = Module(new BRAM(Sample(), addr_width)).io
-  val writeHead = RegNext(0.U(addr_width.W))
+  val mem        = Module(new BRAM(Sample(), addr_width)).io
+  val writeHead  = RegNext(0.U(addr_width.W))
 
-  io.in.ready := true.B
+  writeHead := Mux(io.enable, writeHead + 1.U, writeHead)
 
-  mem.write_enable  := io.in.valid
+  mem.write_enable  := io.enable
   mem.read_addr     := writeHead - io.delaySamples
   mem.write_addr    := writeHead
-  mem.data_in       := io.in.bits
+  mem.data_in       := io.in
 
-  io.out.bits  := mem.data_out
-  io.out.valid := RegNext(io.in.valid)
-
-  writeHead := Mux(io.in.valid, writeHead + 1.U, writeHead)
+  io.out := mem.data_out
 }
--- src/main/scala/effects/Delay.scala
+++ src/main/scala/effects/Delay.scala
@@ -4,7 +4,7 @@ import chisel3._
 import chisel3.util._
 import chisel3.experimental.MultiIOModule
 
-// TODO: Update with bypass
+
 class DelayControl extends Bundle {
   val fbFraction  = Input(new Fraction)
   val mixFraction = Input(new Fraction)
@@ -13,35 +13,30 @@ class DelayControl extends Bundle {
 }
 
 class Delay() extends MultiIOModule {
-
   val io = IO(new EffectBundle)
   val ctrl = IO(new DelayControl)
 
   val delayBuffer = Module(new DelayBuffer).io
 
-
-  delayBuffer.delaySamples := ctrl.delaySamples
-
   // Get the feedback as output of buffer mixed with the input signal
-  val feedback = Multiply(ctrl.fbFraction.numerator, ctrl.fbFraction.denominator, delayBuffer.out.bits) + OneMinusMultiply(ctrl.fbFraction.numerator, ctrl.fbFraction.denominator, io.in.bits)
-
-  // Set io in with control signals, then set feedback to bits
-  delayBuffer.in <> io.in
-  delayBuffer.in.bits := feedback
-
+  val feedback = (Multiply(ctrl.fbFraction.numerator, ctrl.fbFraction.denominator, delayBuffer.out)
+    + OneMinusMultiply(ctrl.fbFraction.numerator, ctrl.fbFraction.denominator, io.in.bits))
 
   // Get the mix of input and output of buffer
-  val mix = Multiply(ctrl.mixFraction.numerator, ctrl.mixFraction.denominator, delayBuffer.out.bits) + OneMinusMultiply(ctrl.mixFraction.numerator, ctrl.mixFraction.denominator, io.in.bits)
+  val mix = (Multiply(ctrl.mixFraction.numerator, ctrl.mixFraction.denominator, delayBuffer.out)
+    + OneMinusMultiply(ctrl.mixFraction.numerator, ctrl.mixFraction.denominator, io.in.bits))
 
+  // Write samples to BRAM, even when the delay is bypassed
+  delayBuffer.in           := feedback
+  delayBuffer.enable       := io.in.valid && io.in.ready
+  delayBuffer.delaySamples := ctrl.delaySamples
 
-  // Set control signals, then send mix out
-  delayBuffer.out <> io.out
+  io.in.ready  := true.B
+  io.out.valid := io.in.valid
 
-  // Send how many delay samples
   when(ctrl.bypass) {
-    io.out.bits := io.in.bits
-  } .otherwise {
+    io.out <> io.in
+  }.otherwise {
     io.out.bits := mix
   }
-
 }
