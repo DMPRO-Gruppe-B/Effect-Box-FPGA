ef9305d2760084197c85f6eb31abd5a3e7f791d4
Ole Ravna <olevra@stud.ntnu.no>
2019-11-19 17:23:17 +0100

    Fix config for wave form

--- src/main/scala/EffectControl.scala
+++ src/main/scala/EffectControl.scala
@@ -5,7 +5,7 @@ import chisel3.experimental.MultiIOModule
 import io.{SPIBus, SPISlave}
 
 class EffectControl extends MultiIOModule {
-  val CONFIG_SIZE = 11
+  val CONFIG_SIZE = 15
 
   val ADDR_BITCRUSH_ENABLE = 0
   val ADDR_BITCRUSH_BITS = 1
@@ -19,7 +19,7 @@ class EffectControl extends MultiIOModule {
   val ADDR_TREMOLO_ENABLE = 5
   val ADDR_TREMOLO_PERIODMULT = 6
   val ADDR_TREMOLO_DEPTH = 9
-  val ADDR_TREMOLO_WAVE = 9
+  val ADDR_TREMOLO_WAVE = 14
 
   val spi = IO(new SPIBus)
   val debug = IO(new Bundle {
@@ -51,10 +51,10 @@ class EffectControl extends MultiIOModule {
 
   /* Tremolo */
   val tremolo = IO(Flipped(new TremoloControl))
-  tremolo.bypass := !(config(ADDR_TREMOLO_ENABLE) & 1.U(1.W))
+  tremolo.bypass := false.B //!(config(ADDR_TREMOLO_ENABLE) & 1.U(1.W))
   tremolo.periodMultiplier := config(ADDR_TREMOLO_PERIODMULT) //18.U
   tremolo.depth := config(ADDR_TREMOLO_DEPTH)
-  tremolo.waveSelect := config(ADDR_TREMOLO_WAVE)
+  tremolo.waveSelect := config(ADDR_TREMOLO_WAVE) // & 0x3.U(2.W)
 
   /* Delay */
   val delay = IO(Flipped(new DelayControl))
