8d0f680567de480289f90cb512c713aed05e6cc2
Håvard O. Nordstrand <haavard@haavard.tech>
2019-11-27 03:40:38 +0100

    Make distortion more granular (requires MCU changes)

--- src/main/scala/effects/Distortion.scala
+++ src/main/scala/effects/Distortion.scala
@@ -10,8 +10,8 @@ import chisel3.util.Decoupled
 
 class DistortionControl extends Bundle {
   val bypass = Input(Bool())
-  // 0-10
-  val amplitude = Input(UInt(4.W))
+  // 0-100
+  val amplitude = Input(UInt(7.W))
 }
 
 class Distortion extends MultiIOModule {
@@ -23,18 +23,18 @@ class Distortion extends MultiIOModule {
 
   io.in.ready := true.B
   io.out.valid := io.in.valid
-  
+
   when (!ctrl.bypass) {
     val sample = io.in.bits
     when (sample >= 0.S) {
-      val maxSample = Multiply(ctrl.amplitude, 10.U, MAX_POS_AMPLITUDE)
+      val maxSample = Multiply(100.U - ctrl.amplitude, 1.U, MAX_POS_AMPLITUDE)
       when (sample > maxSample) {
         io.out.bits := maxSample
       } .otherwise {
         io.out.bits := sample
       }
     } .otherwise {
-      val maxSample = Multiply(ctrl.amplitude, 10.U, MAX_NEG_AMPLITUDE)
+      val maxSample = Multiply(100.U - ctrl.amplitude, 1.U, MAX_NEG_AMPLITUDE)
       when (sample < maxSample) {
         io.out.bits := maxSample
       } .otherwise {
